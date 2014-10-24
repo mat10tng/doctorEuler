be8fc935576622e62af1276e1cd294ead8d65c1e
mat10tng <mat10tng@gmail.com>
2014-10-24 15:12:39 +0200

    modify stuff

---
+++ doctorUtils/doctorNumber/doctorNumber.h
@@ -0,0 +1,18 @@
+#ifndef DOCTOR_NUMBER_H
+#define DOCTOR_NUMBER_H
+
+// we want big int here maybe ? 
+void* reverse_nbr(void* nbr);
+void* dec_to_bin(void* nbr);
+
+void* to_array(void* nbr);
+void* to_array_n_last_digits(void* nbr, size_t size);
+
+void* to_set(void* nbr);
+void* to_number(void* array);
+
+void* nbr_permutations(void* nbr);
+int is_lychrel(void* nbr);
+
+
+#endif
